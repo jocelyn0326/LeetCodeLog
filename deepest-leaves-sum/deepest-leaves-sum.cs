831ccbf55670d4e91b5219cd2ce9b7e4f8047ca9
jocelyn0326 <37055239+jocelyn0326@users.noreply.github.com>
2021-07-10 17:55:53 +0800

    Time: 236 ms (5.40%), Space: 34.4 MB (73.87%) - LeetHub

--- deepest-leaves-sum/deepest-leaves-sum.cs
+++ deepest-leaves-sum/deepest-leaves-sum.cs
@@ -11,45 +11,46 @@
  *     }
  * }
  */
+//
 public class Solution {
     //BFS
-    //public int DeepestLeavesSum(TreeNode root) {
-    //    var queue = new Queue<TreeNode>();
-    //    var result = 0;
-    //    queue.Enqueue(root);
-    //    while(queue.Count > 0){
+    public int DeepestLeavesSum(TreeNode root) {
+        var queue = new Queue<TreeNode>();
+        var result = 0;
+        queue.Enqueue(root);
+        while(queue.Count > 0){
 
-    //        var count = queue.Count;
-    //        result = 0;
+            var count = queue.Count;
+            result = 0;
 
-    //        for (var i = 0; i < count; i++){
-    //            var node = queue.Dequeue();
+            for (var i = 0; i < count; i++){
+                var node = queue.Dequeue();
 
-    //            result += node.val;
-    //            if (node.left != null) queue.Enqueue(node.left);
-    //            if (node.right != null) queue.Enqueue(node.right);
-    //        }                       
-    //    }
-    // return result;
-    //}
+                result += node.val;
+                if (node.left != null) queue.Enqueue(node.left);
+                if (node.right != null) queue.Enqueue(node.right);
+            }                       
+        }
+     return result;
+    }
     
     
     //DFS
-    int deepest =0, sum=0;
-    public int DeepestLeavesSum(TreeNode root, int depth = 0){
-        if(root == null) return 0; // current node is NULL - just return.
-        if(root.left == null && root.left == null)
-        if(depth == deepest){
-            sum += root.val;// if current is a leaf node
+    //int deepest =0, sum=0;
+    //public int DeepestLeavesSum(TreeNode root, int depth = 0){
+    //    if(root == null) return 0; // current node is NULL - just return.
+    //    if(root.left == null && root.left == null)
+    //    if(depth == deepest){
+    //        sum += root.val;// if current is a leaf node
             
-        } 
-        else if(depth> deepest)
-        { 
-            deepest = depth;
-            sum = root.val; // if depth is exactly equal to deepest, add to sum
-        }
-        DeepestLeavesSum(root.left,depth+1);
-        DeepestLeavesSum(root.right,depth+1);
-        return sum;
-    }
+    //    } 
+    //    else if(depth> deepest)
+    //    { 
+     //       deepest = depth;
+     //       sum = root.val; // if depth is exactly equal to deepest, add to sum
+    //    }
+     //   DeepestLeavesSum(root.left,depth+1);
+     //   DeepestLeavesSum(root.right,depth+1);
+     //   return sum;
+    //}
 }
