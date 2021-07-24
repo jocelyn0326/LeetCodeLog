ae1df6a3f363bdff98f7fc6ae82cdd16eca2d660
jocelyn0326 <37055239+jocelyn0326@users.noreply.github.com>
2021-07-24 22:57:51 +0800

    Time: 145 ms (31.46%), Space: 27.3 MB (50.99%) - LeetHub

---
+++ construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode BuildTree(int[] inorder, int[] postorder) {
+        var post = new Stack<int>(postorder);
+        var index = inorder.Select((val, index) => (val, index))
+                           .ToDictionary(x => x.val, x => x.index);
+        return Build(0, inorder.Length - 1);
+
+        TreeNode Build(int left, int right) {
+            if (left > right) return null;
+
+            var val = post.Pop();
+            var i = index[val];
+
+            return new TreeNode(val) {
+                right = Build(i + 1, right),
+                left = Build(left, i - 1)
+            };
+        }
+    }
+}
