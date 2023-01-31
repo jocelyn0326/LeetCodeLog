afa82e101d3bdf2ff0fa96ac59f4ef257d7fe1d6
jocelyn0326 <37055239+jocelyn0326@users.noreply.github.com>
2023-02-01 00:02:12 +0800

    Time: 798 ms (98.98%), Space: 29.9 MB (44.67%) - LeetHub

--- 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.py
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.py
@@ -3,11 +3,7 @@ class Solution:
         killed_count = 0
         time = sorted(d / s for d, s in zip(dist, speed))
         for t in time:
-            if t > killed_count:
-                killed_count +=1
-            else:
+            if t <= killed_count:
                 return killed_count
+            killed_count += 1
         return killed_count
-
-            
-        
