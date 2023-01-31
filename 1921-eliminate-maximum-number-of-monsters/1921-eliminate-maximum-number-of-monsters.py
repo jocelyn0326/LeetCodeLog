a8d0843a9b26c047fa0ed85421c3708fe33a39b1
jocelyn0326 <37055239+jocelyn0326@users.noreply.github.com>
2023-02-01 00:01:15 +0800

    Time: 832 ms (89.34%), Space: 30.3 MB (8.38%) - LeetHub

---
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.py
@@ -0,0 +1,13 @@
+class Solution:
+    def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
+        killed_count = 0
+        time = sorted(d / s for d, s in zip(dist, speed))
+        for t in time:
+            if t > killed_count:
+                killed_count +=1
+            else:
+                return killed_count
+        return killed_count
+
+            
+        
