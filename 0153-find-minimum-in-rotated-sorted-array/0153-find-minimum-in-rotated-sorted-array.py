676dbc1280590a38ccd0703ba200ae85c222bfb4
jocelyn0326 <37055239+jocelyn0326@users.noreply.github.com>
2023-08-09 00:27:05 +0800

    Time: 48 ms (85.02%), Space: 16.7 MB (12.00%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        # We're using binary search to acheive O(log n)
+        # We need to first find out we're in left portion of the arry or the right portion of the array
+        # Algorithm:
+        # If in left: (nums[m] >= l) ->search right: l=m+1 and compare with nums[m]
+        # else: -> search right: r = m-1 and comare with nums[m]
+        res=nums[0]
+        l, r = 0, len(nums)-1
+        
+        # If the array is sorted
+        while l <= r:
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
+            # Do the binary search here
+            m = (l+r) //2
+            res = min(res, nums[m])
+            if nums[m] >= nums[l]:
+                l = m+1
+            else:
+                r = m-1
+        return res
+            
