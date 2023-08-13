5a0b3e78c024aa7dfb80a1949bc155e4092426e8
jocelyn0326 <37055239+jocelyn0326@users.noreply.github.com>
2023-08-13 17:17:58 +0800

    Time: 985 ms (89.05%), Space: 20.5 MB (67.51%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,8 +5,10 @@ class Solution:
         l, r = 0, len(nums)
     
         for i, num in enumerate(nums):
+            # Avoid we return duplicate triplets
             if i > 0 and num == nums[i-1]:
                 continue
+            # Two sum 2
             l , r = i +1, len(nums) -1
             while l < r:
                 ts = num + nums[l] + nums[r]
@@ -16,7 +18,9 @@ class Solution:
                     l += 1
                 else:
                     res.append([num, nums[l], nums[r]])
+                    # Keep finding nex combination
                     l += 1
+                    # Avoid we l still ident to l-1
                     while  nums[l] ==  nums[l-1] and l < r:
                         l += 1
         return res
