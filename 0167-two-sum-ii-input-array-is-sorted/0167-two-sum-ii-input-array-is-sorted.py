e508cf6aca24e000d0e290bddaf053ec0f06d7aa
jocelyn0326 <37055239+jocelyn0326@users.noreply.github.com>
2023-08-13 16:45:19 +0800

    Time: 128 ms (76.24%), Space: 17.3 MB (60.07%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,12 +1,15 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # Since this is a sorted array, so we could take advantage from it
+        # By sum up left and right pointer, we switch the left if sum < target, 
+        # or switch the right pointer if else until we reach the target
         l, r = 0, len(numbers)-1
         while l < r:
-            sum =  numbers[l] + numbers[r]
-            if target == sum:
-                return [l+1 ,r+1]
-            elif sum > target:
-                r -= 1
-            else:
+            sum = numbers[l] + numbers[r]
+            if sum == target:
+                return [ l+1, r+1]
+            elif sum < target:
                 l += 1
-        
+            else:
+                r -= 1
+                
